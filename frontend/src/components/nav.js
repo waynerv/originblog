806b42a4d9abf9358fa58af978251cabed24bd40
Laineyzhang55 <lainey_zhang47@163.com>
2020-07-16 23:49:50 +0800

    revise class components to function components

--- frontend/src/components/nav.js
+++ frontend/src/components/nav.js
@@ -35,49 +35,6 @@ const Close = styled(CloseOutlined)`
 
 const { SubMenu } = Menu;
 
-// class Component extends React.Component {
-//   constructor(props){
-//     super(props)
-//   }
-//   state = {
-//     collapsed: false,
-//   };
-
-//   toggleCollapsed = () => {
-//     this.setState({
-//       collapsed: !this.state.collapsed,
-//     });
-//   };
-
-//   render() {
-//     return (
-//       <Nav style={{ width: 256 }}>
-//         <Close onClick={() => {this.props.router.push(`/`)}}/>
-//         <Button type="primary" onClick={this.toggleCollapsed} style={{ marginBottom: 16 }}>
-//           {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}
-//         </Button>
-//         <Menu
-//           defaultSelectedKeys={['1']}
-//           defaultOpenKeys={['sub1']}
-//           mode="inline"
-//           theme="dark"
-//           inlineCollapsed={this.state.collapsed}
-//         >
-//           <Menu.Item key="1" icon={<ContainerOutlined />}>
-//           <Link to='/view/history'>博客列表</Link>
-//           </Menu.Item>
-//           <SubMenu key="sub1" icon={<AppstoreOutlined />} title="我的博客">
-//             <Menu.Item key="2"> <Link to='/view/editor'>编辑</Link></Menu.Item>
-//           </SubMenu>
-//           <SubMenu key="sub2" icon={<DesktopOutlined />} title="个人信息">
-//             <Menu.Item key="4"><Link to='/view/information'>信息编辑</Link></Menu.Item>
-//           </SubMenu>
-//         </Menu>
-//       </Nav>
-//     );
-//   }
-// }
-
 const Component = observer(()=>{
   let collapsed = false;
   const history = useHistory();
